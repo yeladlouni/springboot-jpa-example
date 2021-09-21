f6a9cf79173aea8e6a8906745da378a2cc45a45f
yassine <yeladlouni@gmail.com>
2021-09-21 19:24:41 +0200

    define schema and data scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO product(id, label) VALUES(1001, 'Printer');
+INSERT INTO product(id, label) VALUES(1002, 'Printer');
+INSERT INTO product(id, label) VALUES(1003, 'Printer');
+INSERT INTO product(id, label) VALUES(1004, 'Printer');
